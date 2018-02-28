8c4acb72a8997d80fb7e5932d6ac79566f72a252
brehantest <brehantest@gmail.com>
2018-02-28 17:02:40 +0100

    ça à l'air de fonctionner

--- test.js
+++ test.js
@@ -7,13 +7,25 @@ Math.random();
 console.log("Nombre aléatoire = ", Math.random());
 
 var nb = prompt("Entrez un nombre entre 0 et 1", nb);
-if (nb != null && nb <= Math.random()) {
-	alert("Votre nombre est inférieur ou égal au nombre aléatoire ");
-} else if (nb != null && nb >= Math.random()) {
-	alert("Votre nombre est supérieur ou égal au nombre aléatoire ");
-} else if (nb != null && nb == Math.random()) {
-	alert("Votre nombre est égal au nombre aléatoire ");
-} else if (nb != null && nb == '') {alert("Vous avez validé sans renseigner de nombre compris entre 0 et 1");}
-	
-
 console.log("Votre nombre est: ", nb);
+
+if (nb != null && nb == '') {alert("Vous avez validé sans renseigner de nombre compris entre 0 et 1");
+console.log("Vous avez validé sans renseigner de nombre compris entre 0 et 1");}
+
+
+else if (nb != null && nb <= Math.random()) {
+	alert("Votre nombre est inférieur ou égal au nombre aléatoire ");
+console.log("Votre nombre est < inférieur ou égal au nombre aléatoire");}
+
+
+
+else if (nb != null && nb >= Math.random()) {
+	alert("Votre nombre est > supérieur ou égal au nombre aléatoire ");
+console.log("Votre nombre est supérieur ou égal au nombre aléatoire");}
+
+
+
+else if (nb != null && nb == Math.random()) {
+alert("Votre nombre est égal au nombre aléatoire ");
+console.log("Votre nombre est égal au nombre aléatoire");}
+
