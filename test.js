55fd2c46e705ae7e14337bb589a63a8c1369997a
brehantest <brehantest@gmail.com>
2018-02-28 18:30:35 +0100

    Suppression de confirm() pour le moment

--- test.js
+++ test.js
@@ -3,34 +3,36 @@ return "bonjour " + prenom ;
 }
 bonjour("laurent")*/
 
-Math.random();
-console.log("Nombre aléatoire = ", Math.random());
+var nombre = Math.random();
+console.log("Nombre aléatoire = ", nombre);
 
-var nb = prompt("Entrez un nombre entre 0 et 1", nb);
+var nb = prompt("Entrez un nombre entre 0 et 1");
 console.log("Votre nombre est: ", nb);
 
-if (nb == null) {confirm("Vous voulez quitter le programme du Nombre aléatoire ?");
+if (nb == null) {alert("Vous quittez le programme du Nombre aléatoire");
+console.log("Vous quittez le programme du Nombre aléatoire");}
+/*if (nb == null) {confirm("Vous voulez quitter le programme du Nombre aléatoire ?");
 console.log("Vous voulez quitter le programme du Nombre aléatoire ?");}
-
 if (confirm = true) {alert("Vous avez quitté le programme du Nombre aléatoire");}
 console.log("Vous avez quitté le programme du Nombre aléatoire");
+if (confirm = false) {alert("Vous restez sur le programme du Nombre aléatoire");}*/
 
 if (nb != null && nb == '') {alert("Vous avez validé sans renseigner de nombre compris entre 0 et 1");
 console.log("Vous avez validé sans renseigner de nombre compris entre 0 et 1");}
 
 
-else if (nb != null && nb < Math.random()) {
+else if (nb != null && nb < nombre) {
 	alert("Votre nombre est inférieur au nombre aléatoire ");
 console.log("Votre nombre est < inférieur au nombre aléatoire");}
 
 
 
-else if (nb != null && nb > Math.random()) {
+else if (nb != null && nb > nombre) {
 	alert("Votre nombre est > supérieur au nombre aléatoire ");
 console.log("Votre nombre est supérieur au nombre aléatoire");}
 
 
 
-else if (nb != null && nb == Math.random()) {
+else if (nb != null && nb == nombre) {
 alert("Votre nombre est égal au nombre aléatoire ");
 console.log("Votre nombre est égal au nombre aléatoire");}
