feed01af6874c7f027cb932ae3ba4899d759d58b
brehantest <brehantest@gmail.com>
2018-02-28 15:42:52 +0100

    1er jet

--- test.js
+++ test.js
@@ -3,7 +3,17 @@ return "bonjour " + prenom ;
 }
 bonjour("laurent")*/
 
-function nbAleatoire() {
-	var A = 25
-	var B = 1
-}
+Math.random();
+console.log("Nombre aléatoire = ", Math.random());
+
+var nb = prompt("Entrez un nombre entre 0 et 1", nb);
+if (nb != null && nb <= Math.random()) {
+	alert("Votre nombre est inférieur ou égal au nombre aléatoire ");
+} else if (nb != null && nb >= Math.random()) {
+	alert("Votre nombre est supérieur ou égal au nombre aléatoire ");
+} else if (nb != null && nb == Math.random()) {
+	alert("Votre nombre est égal au nombre aléatoire ");
+} else if (nb != null && nb == '') {alert("Vous avez validé sans renseigner de nombre compris entre 0 et 1");}
+	
+
+console.log("Votre nombre est: ", nb);
